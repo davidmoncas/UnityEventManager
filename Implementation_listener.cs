c62328ea3b1f8d048fb59f99f3618887a49c841f
David Montoya Castaño <30814399+davidmoncas@users.noreply.github.com>
2024-01-17 17:55:24 +0100

    Update Implementation_listener.cs

--- Implementation_listener.cs
+++ Implementation_listener.cs
@@ -11,15 +11,15 @@ public class Implementation_listener : MonoBehaviour
     {
         // Subscribe to the desired events, remember to specify the signature of the method
         EventManager.Subscribe(Events.Gameplay.Event1, (System.Action)SomeMethod);
-        EventManager.Subscribe(Events.Gameplay.Event1, (System.Action<int>)SomeMethod2);
-        EventManager.Subscribe(Events.Gameplay.Event1, (System.Action<int , string>)SomeMethod3);
+        EventManager.Subscribe(Events.Gameplay.Event2, (System.Action<int>)SomeMethod2);
+        EventManager.Subscribe(Events.Gameplay.Event3, (System.Action<int , string>)SomeMethod3);
     }
 
     private void OnDisable()
     {
         // Remember to unsubscribe to the events 
         EventManager.Unsubscribe(Events.Gameplay.Event1, (System.Action)SomeMethod);
-        EventManager.Unsubscribe(Events.Gameplay.Event1, (System.Action<int>)SomeMethod2);
-        EventManager.Unsubscribe(Events.Gameplay.Event1, (System.Action<int, string>)SomeMethod3);
+        EventManager.Unsubscribe(Events.Gameplay.Event2, (System.Action<int>)SomeMethod2);
+        EventManager.Unsubscribe(Events.Gameplay.Event3, (System.Action<int, string>)SomeMethod3);
     }
 }
