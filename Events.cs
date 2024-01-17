7d3c335397767611609b70f9e21fce2d053a5c79
David Montoya Castaño <30814399+davidmoncas@users.noreply.github.com>
2024-01-17 17:43:05 +0100

    Create Events.cs

---
+++ Events.cs
@@ -0,0 +1,25 @@
+public static class Events 
+{
+    public enum Gameplay{
+        Event1,
+        Event2,
+        Event3,
+        // ... Add all the events you want
+    }
+
+    public enum UI{
+        Event1,
+        Event2,
+        Event3,
+        // ... Add all the events you want
+    }
+
+      public enum VFX{
+        Event1,
+        Event2,
+        Event3,
+        // ... Add all the events you want
+    }
+
+  // Separate the events in different enums to be more organized
+}
